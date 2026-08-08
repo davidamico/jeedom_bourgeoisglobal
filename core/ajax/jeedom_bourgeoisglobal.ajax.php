6f1402c7689ede94e075ed3340cb7a3f73f0fc25
TheDave <amico.da@gmail.com>
2026-08-08 13:59:53 +0200

    Update jeedom_bourgeoisglobal.ajax.php

--- core/ajax/jeedom_bourgeoisglobal.ajax.php
+++ core/ajax/jeedom_bourgeoisglobal.ajax.php
@@ -1,1 +1,18 @@
+<?php
+try {
+    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+    include_file('core', 'authentification', 'php');
 
+    if (!isConnect('admin')) {
+        throw new Exception(__('401 - Accès non autorisé', __FILE__));
+    }
+
+    if (init('action') == 'testConnection') {
+        // Code pour tester les identifiants Bourgeois Global
+        ajax::success(array('message' => __('Connexion réussie !', __FILE__)));
+    }
+
+    throw new Exception(__('Aucune action correspondante : ', __FILE__) . init('action'));
+} catch (Exception $e) {
+    ajax::error(displayException($e), $e->getCode());
+}
