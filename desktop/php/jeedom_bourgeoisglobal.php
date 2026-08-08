2f68905964a42ca735777f9bbdd78e00feebf0b6
TheDave <amico.da@gmail.com>
2026-08-08 14:20:05 +0200

    Update jeedom_bourgeoisglobal.php

--- desktop/php/jeedom_bourgeoisglobal.php
+++ desktop/php/jeedom_bourgeoisglobal.php
@@ -8,16 +8,28 @@ $eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
-    <div class="col-xs-12 eqLogicThumbnailDisplay">
-        <legend><i class="fas fa-cog"></i> {{Gestion du plugin}}</legend>
+    <div class="col-xs-12 col-md-3 col-lg-2 eqLogicThumbnailDisplay">
+        <legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
         <div class="eqLogicThumbnailContainer">
             <div class="cursor eqLogicAction logoPrimary" data-action="add">
                 <i class="fas fa-plus-circle"></i>
                 <br>
                 <span>{{Ajouter}}</span>
             </div>
+            <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+                <i class="fas fa-wrench"></i>
+                <br>
+                <span>{{Configuration}}</span>
+            </div>
         </div>
-        <legend><i class="fas fa-solar-panel"></i> {{Mes Equipements Bourgeois Global}}</legend>
+
+        <legend><i class="fas fa-solar-panel"></i> {{Mes Équipements}}</legend>
+        <div class="input-group" style="margin-bottom: 5px;">
+            <input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>
+            <div class="input-group-btn">
+                <a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>
+            </div>
+        </div>
         <div class="eqLogicThumbnailContainer">
             <?php
             foreach ($eqLogics as $eqLogic) {
@@ -32,7 +44,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
         </div>
     </div>
 
-    <div class="col-xs-12 eqLogic" style="display: none;">
+    <div class="col-xs-12 col-md-9 col-lg-10 eqLogic" style="display: none;">
         <div class="input-group pull-right" style="display:inline-flex">
             <span class="input-group-btn">
                 <a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a>
@@ -41,10 +53,16 @@ $eqLogics = eqLogic::byType($plugin->getId());
                 <a class="btn btn-danger btn-sm eqLogicAction" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
             </span>
         </div>
+
         <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Équipement}}</a></li>
-            <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
+            <li role="presentation" class="active">
+                <a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Équipement}}</a>
+            </li>
+            <li role="presentation">
+                <a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a>
+            </li>
         </ul>
+
         <div class="tab-content">
             <div role="tabpanel" class="tab-pane active" id="eqlogictab">
                 <br/>
@@ -54,9 +72,10 @@ $eqLogics = eqLogic::byType($plugin->getId());
                             <label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
                             <div class="col-sm-4">
                                 <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de votre centrale solaire}}"/>
                             </div>
                         </div>
+
                         <div class="form-group">
                             <label class="col-sm-3 control-label">{{Objet parent}}</label>
                             <div class="col-sm-4">
@@ -72,27 +91,71 @@ $eqLogics = eqLogic::byType($plugin->getId());
                                 </select>
                             </div>
                         </div>
+
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Catégorie}}</label>
+                            <div class="col-sm-9">
+                                <?php
+                                foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                                    echo '<label class="checkbox-inline">';
+                                    echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                                    echo '</label>';
+                                }
+                                ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Options}}</label>
+                            <div class="col-sm-9">
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}
+                                </label>
+                            </div>
+                        </div>
+
+                        <hr>
+                        <legend><i class="fas fa-key"></i> {{Paramètres de connexion Bourgeois Global / Hoymiles}}</legend>
+
                         <div class="form-group">
                             <label class="col-sm-3 control-label">{{Identifiant / Email Cloud}}</label>
                             <div class="col-sm-4">
                                 <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="username" placeholder="exemple@email.com"/>
                             </div>
                         </div>
+
                         <div class="form-group">
                             <label class="col-sm-3 control-label">{{Mot de passe Cloud}}</label>
                             <div class="col-sm-4">
-                                <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="password"/>
+                                <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="password" placeholder="Mot de passe"/>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{ID de la Station / Centrale}}</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="station_id" placeholder="Ex: 123456 (Optionnel)"/>
+                                <span class="help-block"><small>{{Laissez vide si vous n'avez qu'une seule installation sur votre compte}}</small></span>
                             </div>
                         </div>
                     </fieldset>
                 </form>
             </div>
+
             <div role="tabpanel" class="tab-pane" id="commandtab">
                 <br/>
                 <table id="table_cmd" class="table table-bordered table-condensed">
                     <thead>
                         <tr>
-                            <th>{{Nom}}</th><th>{{Type}}</th><th>{{Options}}</th><th>{{Action}}</th>
+                            <th style="width: 50px;">#</th>
+                            <th style="width: 300px;">{{Nom}}</th>
+                            <th style="width: 200px;">{{Type}}</th>
+                            <th style="width: 150px;">{{Unité}}</th>
+                            <th>{{Options}}</th>
+                            <th style="width: 100px;">{{Action}}</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -102,5 +165,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
         </div>
     </div>
 </div>
+
 <?php include_file('desktop', 'jeedom_bourgeoisglobal', 'js', 'jeedom_bourgeoisglobal');?>
 <?php include_file('core', 'plugin.template', 'js'); ?>
